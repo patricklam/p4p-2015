2d3388a615fa5ce964f186faebc6a6b77f38a184
David Cheng <david.cheng@uwaterloo.ca>
2015-01-19 23:00:28 -0500

    change of folder

--- tutorials/t2/http_server_select.h
+++
@@ -1,9 +0,0 @@
-#include "http_server_base.h"
-
-class HTTPServerSelect : public HTTPServerBase {
- public:
-	void start_serving();
-
-	HTTPServerSelect( int port ) : 
-		HTTPServerBase( port ) { }
-};
