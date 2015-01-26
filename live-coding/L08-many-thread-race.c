4e41c2fd8a8f3d90a8a2d86764d71023f15ac916
Patrick Lam <prof.lam@gmail.com>
2015-01-26 14:16:55 -0500

    Examples from live coding

--- live-coding/L08-many-thread-race.c
+++ live-coding/L08-many-thread-race.c
@@ -1,34 +1,60 @@
 // credit: Josh
 // observed output: 108
 
+// I've modified this to use different locking mechanisms.
+// Run & time it with different compile flags to test that out.
+// To observe time differences, `i' should go to something like 10000000, not 100.
+// Raw compilation is racy and is fastest.
+// Atomics are slightly slower.
+// Mutexes are slowest.
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <thread>
+#ifdef MUTEX
 #include <mutex>
+#endif
+#ifdef ATOMIC
+#include <atomic>
+#endif
 #include <unistd.h>
 #include <iostream>
 
 #define MAX_THREADS 8
 
+#ifdef ATOMIC
+static std::atomic<int> counter;
+#else
 static int counter = 0;
+#endif
 
+#ifdef MUTEX
 std::mutex m;
+#endif
 
 void loop()
 {
     for (int i = 0; i < 100; i++)
     {
+#ifdef MUTEX
+        m.lock();
+#endif
+#ifndef ATOMIC
         int j = counter;
-        j++;
-        
-        if (i % 10 == 0)
-            sleep(1);
+        j++;        
         counter = j;
+#else
+        counter++;
+#endif
+#ifdef MUTEX
+        m.unlock();
+#endif
     }
 }
 
 int main() {
     std::thread *tids[MAX_THREADS];
+    counter = 0;
     for (int i = 0; i < MAX_THREADS; i++)
     {
         tids[i] = new std::thread(loop);
@@ -38,5 +64,5 @@ int main() {
     {
         tids[j]->join();
     }
-    std::cout << counter;
+    std::cout << counter << "\n";
 }
--- live-coding/L10-gimple-example.c
+++ live-coding/L10-gimple-example.c
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 
 int main(){
-   int foo = 2;
-   int bar = foo + 8;
-   int res = bar%foo + bar;
-   if(res == 0){
-	res++;
-   }
-   printf("This is empty %n", bar);
+    int i, j, k;
+    scanf("%d\n", &i);
+    j = i;
+    k = j * i + j;
+    if (k + j == 2) {
+        printf("%d\n", k);
+    }
+    j *= i;
 }
